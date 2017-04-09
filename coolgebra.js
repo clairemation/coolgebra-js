c28faf64d3b644c8bcc0b99aee64c2a99cf5a6d4
Claire Samuels <clairemation@gmail.com>
2017-04-09 13:20:10 -0400

    Add files via upload

--- coolgebra.js
+++ coolgebra.js
@@ -3,19 +3,24 @@ const END_TAG = '$';
 const Stack = require('./stack'),
   {sin, cos} = Math,
   vectorInstance = new Vector(),
-  values = new Stack();
+  values = new Stack(),
+  sizeStack = new Stack();
+sizeStack.push(0);
 var size = 0;
 
 function Vector(){};
 
 var $ = function(v){
   values.push(v);
-  size = v.length;
+  // sizeStack.push(v.length);
+  // size = sizeStack.peek();
   return vectorInstance;
 };
 
 Object.defineProperty(Vector.prototype, END_TAG, {
   get: function(){
+    // sizeStack.pop();
+    // size = sizeStack.peek();
     return values.pop();
   }
 });
@@ -32,6 +37,7 @@ Vector.prototype.plus = function(x){
 
 Vector.prototype.plusScalar = function(s){
   var v = values.pop();
+  var size = v.length;
 
   // Selects appropriate version for vector/matrix size--no logic branching, no loops, lazily evaluated, for optimal(?) performance
   var functionVersion = {
@@ -51,6 +57,7 @@ Vector.prototype.plusScalar = function(s){
 
 Vector.prototype.plusVector = function(v){
   var u = values.pop();
+  var size = u.length;
   var functionVersion = {
     2: function(){
       return [u[0]+v[0], u[1]+v[1]];
@@ -68,6 +75,7 @@ Vector.prototype.plusVector = function(v){
 
 Vector.prototype.minusVector = function(v){
   var u = values.pop();
+  var size = u.length;
   var functionVersion = {
     2: function(){
       return [u[0]-v[0], u[1]-v[1]];
@@ -85,6 +93,7 @@ Vector.prototype.minusVector = function(v){
 
 Vector.prototype.timesScalar = function(s){
   var v = values.pop();
+  var size = v.length;
   var functionVersion = {
     2: function(){
       return [v[0]*s, v[1]*s];
@@ -102,6 +111,7 @@ Vector.prototype.timesScalar = function(s){
 
 Vector.prototype.timesVector = function(v){
   var u = values.pop();
+  var size = u.length;
   var functionVersion = {
     2: function(){
       return [u[0]*v[0], u[1]*v[1]];
@@ -112,47 +122,65 @@ Vector.prototype.timesVector = function(v){
     4: function(){
       return [u[0]*v[0], u[1]*v[1], u[2]*v[2], u[3]*v[3]];
     },
-    16: {
-      4: function(){
-        return [
-          u[0]*v[0], u[1]*v[1], u[2]*v[2], u[3]*v[3],
-          u[4]*v[0], u[5]*v[1], u[6]*v[2], u[7]*v[3],
-          u[8]*v[0], u[9]*v[1], u[10]*v[2],u[11]*v[3],
-          u[12]*v[0],u[13]*v[1],u[14]*v[2],u[15]*v[3]
-        ];
-      },
-      16: function(){
-        return [
-          u[0]*v[0] + u[1]*v[4] + u[2]*v[8] + u[3]*v[12],
-          u[0]*v[1] + u[1]*v[5] + u[2]*v[9] + u[3]*v[13],
-          u[0]*v[2] + u[1]*v[6] + u[2]*v[10] + u[3]*v[14],
-          u[0]*v[3] + u[1]*v[7] + u[2]*v[11] + u[3]*v[15],
 
-          u[4]*v[0] + u[5]*v[4] + u[6]*v[8] + u[7]*v[12],
-          u[4]*v[1] + u[5]*v[5] + u[6]*v[9] + u[7]*v[13],
-          u[4]*v[2] + u[5]*v[6] + u[6]*v[10] + u[7]*v[14],
-          u[4]*v[3] + u[5]*v[7] + u[6]*v[11] + u[7]*v[15],
+  }[v.length];
+  values.push(functionVersion[size]());
+  return this;
+}
 
-          u[8]*v[0] + u[9]*v[4] + u[10]*v[8] + u[11]*v[12],
-          u[8]*v[1] + u[9]*v[5] + u[10]*v[9] + u[11]*v[13],
-          u[8]*v[2] + u[9]*v[6] + u[10]*v[10] + u[11]*v[14],
-          u[8]*v[3] + u[9]*v[7] + u[10]*v[11] + u[11]*v[15],
+Vector.prototype.timesMatrix = function(m){
+  var v = values.pop();
+  var size = v.length;
+  var functionVersion = {
+    3: function(){
+      return [
+        m[0]*v[0] + m[1]*v[1] + m[2]*v[2],
+        m[3]*v[0] + m[4]*v[1] + m[5]*v[2],
+        m[6]*v[0] + m[7]*v[1] + m[8]*v[2],
+      ];
+    },
+    4: function(){
+      return [
+        m[0]*v[0] + m[1]*v[1] + m[2]*v[2] + m[3]*v[3],
+        m[4]*v[0] + m[5]*v[1] + m[6]*v[2] + m[7]*v[3],
+        m[8]*v[0] + m[9]*v[1] + m[10]*v[2] + m[11]*v[3],
+        m[12]*v[0] + m[13]*v[1] + m[14]*v[2] + m[15]*v[3]
+      ];
+    },
+    16: function(){
+      return [
+        m[0]*v[0] + m[1]*v[4] + m[2]*v[8] + m[3]*v[12],
+        m[0]*v[1] + m[1]*v[5] + m[2]*v[9] + m[3]*v[13],
+        m[0]*v[2] + m[1]*v[6] + m[2]*v[10] + m[3]*v[14],
+        m[0]*v[3] + m[1]*v[7] + m[2]*v[11] + m[3]*v[15],
 
-          u[12]*v[0] + u[13]*v[4] + u[14]*v[8] + u[15]*v[12],
-          u[12]*v[1] + u[13]*v[5] + u[14]*v[9] + u[15]*v[13],
-          u[12]*v[2] + u[13]*v[6] + u[14]*v[10] + u[15]*v[14],
-          u[12]*v[3] + u[13]*v[7] + u[14]*v[11] + u[15]*v[15]
-        ];
-      }
-    }[v.length]
+        m[4]*v[0] + m[5]*v[4] + m[6]*v[8] + m[7]*v[12],
+        m[4]*v[1] + m[5]*v[5] + m[6]*v[9] + m[7]*v[13],
+        m[4]*v[2] + m[5]*v[6] + m[6]*v[10] + m[7]*v[14],
+        m[4]*v[3] + m[5]*v[7] + m[6]*v[11] + m[7]*v[15],
+
+        m[8]*v[0] + m[9]*v[4] + m[10]*v[8] + m[11]*v[12],
+        m[8]*v[1] + m[9]*v[5] + m[10]*v[9] + m[11]*v[13],
+        m[8]*v[2] + m[9]*v[6] + m[10]*v[10] + m[11]*v[14],
+        m[8]*v[3] + m[9]*v[7] + m[10]*v[11] + m[11]*v[15],
+
+        m[12]*v[0] + m[13]*v[4] + m[14]*v[8] + m[15]*v[12],
+        m[12]*v[1] + m[13]*v[5] + m[14]*v[9] + m[15]*v[13],
+        m[12]*v[2] + m[13]*v[6] + m[14]*v[10] + m[15]*v[14],
+        m[12]*v[3] + m[13]*v[7] + m[14]*v[11] + m[15]*v[15]
+      ];
+    }
   };
   values.push(functionVersion[size]());
   return this;
 }
 
+
 Vector.prototype.times = function(...x){
   if (x.length === 1){
     return this.timesScalar(x)
+  } else if (x.length === 16) {
+    return this.timesMatrix(...x);
   } else {
     return this.timesVector(...x);
   }
@@ -160,6 +188,7 @@ Vector.prototype.times = function(...x){
 
 Vector.prototype.divideByScalar = function(s){
   var v = values.pop();
+  var size = v.length;
   var functionVersion = {
     2: function(){
       return [v[0]/s, v[1]/s];
@@ -177,6 +206,7 @@ Vector.prototype.divideByScalar = function(s){
 
 Vector.prototype.divideByVector = function(v){
   var u = values.pop();
+  var size = u.length;
   var functionVersion = {
     2: function(){
       return [u[0]/v[0], u[1]/v[1]];
@@ -193,15 +223,16 @@ Vector.prototype.divideByVector = function(v){
 }
 
 Vector.prototype.divideBy = function(x){
-  if (x.length === 1){
-    return this.divideByScalar(x)
-  } else {
+  if (Array.isArray(x)){
     return this.divideByVector(x);
+  } else {
+    return this.divideByScalar(x);
   }
 };
 
 Vector.prototype.dot = function(v){
   var u = values.pop();
+  var size = u.length;
   var functionVersion = {
     2: function(){
       return u[0]*v[0] + u[1]*v[1];
@@ -234,20 +265,44 @@ Vector.prototype.length = function(){
   return this;
 }
 
-Vector.prototype.squaredDistance = function(v){
-  values.push(this.minusVector(v).squaredLength()[END_TAG]);
+Vector.prototype.distanceTo = function(b){
+  var a = values.pop();
+  var vec = $(b).minusVector(a).$;
+  values.push($(vec).length().$);
   return this;
 }
 
-Vector.prototype.distance = function(v){
-  values.push(Math.sqrt(this.squaredDistance(v)[END_TAG]));
+Vector.prototype.squaredDistanceTo = function(b){
+  var a = values.pop();
+  var vec = $(b).minusVector(a).$;
+  values.push($(vec).squaredLength().$);
+  return this;
+}
+
+Vector.prototype.nearest = function(a, b){
+  var v = values.pop();
+  var distA = $(v).squaredDistanceTo(a).$;
+  var distB = $(v).squaredDistanceTo(b).$;
+  values.push( distA < distB ? a : b);
+  return this;
+}
+
+Vector.prototype.isLeftOf = function(r){
+  var v = values.pop();
+  values.push((v[1]-r[1])*(r[2]-r[0]) > (v[0]-r[0])*(r[3]-r[2]));
   return this;
 }
 
 Vector.prototype.unit = function(){
-  values.push(
-    this.divideByScalar($(values.peek()).length()[END_TAG])[END_TAG]
-  );
+  var value = values.peek();
+  if (value[0] === 0 && value[1] === 0 && value[2] === 0) {
+    values.pop();
+    values.push([0,0,0]);
+  } else {
+    values.push(
+      this.divideByScalar($(values.peek()).length()[END_TAG])[END_TAG]
+    );
+  }
   return this;
 }
 
@@ -264,15 +319,20 @@ Vector.prototype.turnRight = function(){
 }
 
 Vector.prototype.leftNormal = function(){
-  return this.turnLeft().unit();
+  var v = values.pop();
+  values.push($(v).unit().turnLeft()[END_TAG]);
+  return this;
 }
 
 Vector.prototype.rightNormal = function(){
-  return this.turnRight().unit();
+  var v = values.pop();
+  values.push($(v).unit().turnRight()[END_TAG]);
+  return this;
 }
 
 Vector.prototype.rotate2d = function(angle){
-  var v = values.pop()
+  var v = values.pop();
+  var size = v.length;
   v[0] = v[0] * cos(angle) - v[1] * sin(angle);
   v[1] = v[0] * sin(angle) + v[1] * cos(angle);
   values.push(v);
@@ -281,25 +341,34 @@ Vector.prototype.rotate2d = function(angle){
 
 Vector.prototype.angle2d = function(){
   var v = values.pop();
+  var size = v.length;
   values.push(Math.atan2(v[1], v[0]));
   return this;
 }
 
 Vector.prototype.directionTo = function(v){
   var u = values.pop();
-  values.push(v);
+  var size = u.length;
   values.push(this.minusVector(u).unit()[END_TAG]);
   return this;
 }
 
 Vector.prototype.projectedLength = function(v){
-  values.push(this.dot(v)[END_TAG]);
+  values.push(Math.sqrt(Math.abs(this.dot(v)[END_TAG])));
+  return this;
+}
+
+Vector.prototype.scalarProjection = function(b){
+  var a = values.pop();
+  var bLength = $(b).length().$;
+  values.push(bLength === 0 ? 0 : $(a).dot(b).$ / $(b).length().$);
   return this;
 }
 
 // Todo: Add versions for other size matrices
 Vector.prototype.transpose = function(){
   var v = values.pop();
+  var size = v.length;
   values.push([
     v[0], v[4], v[8], v[12],
     v[1], v[5], v[9], v[13],
@@ -309,17 +378,9 @@ Vector.prototype.transpose = function(){
   return this;
 }
 
-Vector.prototype.scale = function(x,y,z){
-  values.push(this.times([
-    x, 0, 0, 0,
-    0, y, 0, 0,
-    0, 0, z, 0,
-    0, 0, 0, 1
-  ])[END_TAG]);
-  return this;
-};
-
-Vector.prototype.rotate = function(x,y,z){
+Vector.prototype.rotate = function([x, y, z]){
+  var v = values.pop();
+  var size = v.length;
   if (size === 2){
     values.push(this.rotate2d(x)[END_TAG]);
     return this;
@@ -342,14 +403,74 @@ Vector.prototype.rotate = function(x,y,z){
       0, 0, 1, 0,
       0, 0, 0, 1
     ];
+  values.push($(v).timesMatrix(xRotation).timesMatrix(yRotation).timesMatrix(zRotation)[END_TAG]);
+  return this;
+}
 
-  values.push(this.times(xRotation).times(yRotation).times(zRotation)[END_TAG]);
+Vector.prototype.cross = function(b){
+  var a = values.pop();
+  var size = a.length;
+  values.push([
+    a[1]*b[2] - a[2]*b[1],
+    a[2]*b[0] - a[0]*b[2],
+    a[0]*b[1] - a[1]*b[0]
+  ]);
+  return this;
+}
+
+Vector.prototype.coordPairToVector = function(){
+  var coords = values.pop();
+  values.push($([coords[2], coords[3]]).minusVector([coords[0], coords[1]]).$);
+  return this;
+}
+
+Vector.prototype.angleTo = function(b){
+  var a = values.pop();
+  values.push(Math.acos($(a).dot(b).$));
+  return this;
+}
+
+Vector.prototype.rotateToPlane = function(planeNormal, myAxis = [0,1,0]){
+  var v = values.pop();
+  var rotationMatrix = [];
+  if (planeNormal[0] != 0 || planeNormal[1] != 0 || planeNormal[2] != 0) {
+    var angle = Math.acos($(myAxis).dot(planeNormal).$),
+      axis = $(myAxis).cross(planeNormal).unit().$,
+      c = cos(angle),
+      t = 1 - c,
+      s = sin(angle),
+      [x, y, z] = axis,
+      tX = t*x,
+      tY = t*y,
+      tZ = t*z,
+      tXY = tX * y,
+      tYX = tXY,
+      tXX = tX * x,
+      tYZ = tY * z,
+      tZY = tYZ,
+      tYY = tY * y,
+      tXZ = tX * z,
+      tZX = tXZ,
+      tZZ = tZ * z,
+      xS = x * s,
+      yS = y * s,
+      zS = z * s,
+    rotationMatrix = [
+      tXX + c,   tYX - zS,  tZX + yS,
+      tXY + zS,  tYY + c,   tZY - xS,
+      tXZ + yS,  tYZ + xS,  tZZ + c
+    ];
+  } else {
+    rotationMatrix = [1,0,0, 0,1,0, 0,0,1];
+  }
+  values.push($(v).timesMatrix(rotationMatrix).$);
   return this;
 }
 
 Vector.prototype.inverse = function(){
   var r = Vector.IDENTITY_MATRIX,
     m = values.pop();
+    var size = m.length;
 
   r[0] = m[5]*m[10]*m[15] - m[5]*m[14]*m[11] - m[6]*m[9]*m[15] + m[6]*m[13]*m[11] + m[7]*m[9]*m[14] - m[7]*m[13]*m[10];
   r[1] = -m[1]*m[10]*m[15] + m[1]*m[14]*m[11] + m[2]*m[9]*m[15] - m[2]*m[13]*m[11] - m[3]*m[9]*m[14] + m[3]*m[13]*m[10];
